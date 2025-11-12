33bb1f49a67cc9b5fd576439a23bae885bc14e26
Justin Rajewski <justin@alchitry.com>
2025-11-12 11:12:00 -0700

    Updated test version number

--- tests/lib.rs
+++ tests/lib.rs
@@ -15,24 +15,22 @@ mod tests {
         //let expected_version: u32 = 0x0;
         cfg_if::cfg_if! {
             if #[cfg(all(target_os = "linux", target_arch = "x86_64"))] {
-                // version "1.0.5" is represented as 0x010005
-                // let expected_version = 0x01_00_05;
-                // for some reason 1.0.5 reports as 0x1000016
-                let expected_version = 0x1_00_00_16;
+                // for some reason 1.1.0 reports as 0x1000015
+                let expected_version = 0x1_00_00_15;
             } else if #[cfg(all(target_os = "linux", target_arch = "x86"))] {
                 // version "1.0.5" is represented as 0x010005
                 let expected_version = 0x01_00_05;
             } else if #[cfg(all(target_os = "windows", target_arch = "x86_64"))] {
-                // version "1.3.0.4" is represented as 0x1030004
-                let expected_version = 0x1_03_00_04;
+                // version "1.4.0.1" is represented as 0x1040001
+                let expected_version = 0x1_04_00_01;
             } else if #[cfg(all(target_os = "windows", target_arch = "x86"))] {
-                let expected_version = 0x1_03_00_04;
+                let expected_version = 0x1_04_00_01;
             } else if #[cfg(all(target_os = "linux", target_arch = "arm"))] {
-                // version "1.0.5" is represented as 0x010005
-                let expected_version = 0x01_00_05;
+                // for some reason 1.1.0 reports as 0x1000015
+                let expected_version = 0x1_00_00_15;
             } else if #[cfg(all(target_os = "linux", target_arch = "aarch64"))] {
-                // version "1.0.5" is represented as 0x010005
-                let expected_version = 0x01_00_05;
+                // for some reason 1.1.0 reports as 0x1000015
+                let expected_version = 0x1_00_00_15;
             } else if #[cfg(all(target_os = "macos", target_arch = "x86_64"))] {
                 // version "1.0.5" is represented as 0x010005
                 let expected_version = 0x01_00_05;
@@ -43,6 +41,7 @@ mod tests {
                 std::compile_error!("pre-generated bindings are not avaliable for your target");
             }
         };
+        println!("libftd3xx version: {version}");
         assert_eq!(version, expected_version);
     }
 
